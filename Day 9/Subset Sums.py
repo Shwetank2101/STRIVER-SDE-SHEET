f24b3595439eb7d96ac6c5fb6a4c9423631c8040
Shwetank2101 <Shwetankdixitofficial@gmail.com>
2021-07-08 15:41:40 +0530

    Day 9

--- Day 9/Subset Sums.py
+++ Day 9/Subset Sums.py
@@ -1,3 +1,22 @@
+# Recursion
+class Solution:
+    def isSubsetSum (self, N, arr, sum):
+        check=[[0 for i in range(sum+1)] for j in range(N+1)]
+        for i in range(N+1):
+            check[i][0]=1
+        for i in range(1,sum+1):
+            check[0][i]=0
+        for i in range(1,N+1):
+            for j in range(1,sum+1):
+                if j<arr[i-1]:
+                    check[i][j]=check[i-1][j]
+                else:
+                    check[i][j]=( check[i-1][j] or  check[i-1][j-arr[i-1]])
+        return check[-1][-1]
+
+
+# DP
+'''
 def check(l,x,n):
     if x<0 or n<0:
         return False
@@ -7,3 +26,4 @@ def check(l,x,n):
 l=list(map(int,input().split()))
 x=int(input())
 print(check(l,x,len(l)))
+
