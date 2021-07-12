5de3cf3932331a66ca1bd51201efd01c0fa0aac4
Shwetank2101 <Shwetankdixitofficial@gmail.com>
2021-07-12 15:57:05 +0530

    Day 20

---
+++ Day20/Search given Key in BST.py
@@ -0,0 +1,10 @@
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return 
+        elif root.val==val:
+            return root
+        elif root.val<val:
+            return self.searchBST(root.right,val)
+        else:
+            return self.searchBST(root.left,val)
